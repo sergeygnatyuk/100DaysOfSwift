4bbd8a9b58fbc28bf1f95ec9e4ea0be32365ace9
Sergey <Starcom12@mail.ru>
2021-04-05 16:47:19 +0300

    First commit

---
+++ Milestone: Projects 1-3/Milestone: Projects 1-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone: Projects 1-3
+//
+//  Created by Гнатюк Сергей on 05.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
