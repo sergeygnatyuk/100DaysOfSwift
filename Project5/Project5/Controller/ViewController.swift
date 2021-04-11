a5ac6ebb5bb6c991795d77bc2f680ed2e1e01389
Sergey <Starcom12@mail.ru>
2021-04-11 13:54:08 +0300

    Restructure

--- Project5/Project5/Controller/ViewController.swift
+++ Project5/Project5/Controller/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        //homework method restart game with save all word in tableView
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -28,7 +30,7 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -57,33 +59,30 @@ class ViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
-        let errorTitle: String
-        let errorMessage: String
-        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
+                    if isShort(word: lowerAnswer) {
+                    //bonus homework elimination error uppercased
+                        usedWords.insert(answer.lowercased(), at: 0)
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
                     return
-                }else{
-                    errorTitle = "Word not recognized"
-                    errorMessage = "You can't just make them up, you know"
+                    } else {
+                        showErrorMessage(message: "Word shorter than three letters or are just our start word", title: "Think a little and write again")
+                    }
+                } else {
+                    showErrorMessage(message: "Word not recognized", title: "You can't just make them up, you know")
                 }
             } else {
-                errorTitle = "Word already used"
-                errorMessage = "Be more original!"
+                showErrorMessage(message: "Word already used", title: "Be more original!")
             }
-        }else{
-            guard let title = title else {return}
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title.lowercased())."
+        } else {
+            guard let title = title else { return }
+            showErrorMessage(message: "Word not possible", title: "You can't spell that word from \(title.lowercased()).")
         }
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        
     }
     
     func isPossible(word: String) -> Bool {
@@ -104,10 +103,28 @@ class ViewController: UITableViewController {
     }
     
     func isReal(word: String) -> Bool {
+        
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         return misspelledRange.location == NSNotFound
+    }
+    // homework checking for the number of letters and matching the init word
+    func isShort(word: String) -> Bool {
+        if title!.starts(with: word) && word.count < 3 {
+            return false
+        } else {
+        return true
+        }
+    }
+//homework method for refactoring
+    func showErrorMessage(message: String, title: String) {
+        let errorTitle = title
+        let errorMessage = message
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 }
 
