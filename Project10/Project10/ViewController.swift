3814408477d7563104e1bbb9160f3a4d78191c3a
Sergey <Starcom12@mail.ru>
2021-04-26 23:49:11 +0300

    Finished project 10, add alertcontroller for delete person and checking the ability to use the camera

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -43,6 +43,12 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     @objc func newPerson() {
         let picker = UIImagePickerController()
         picker.allowsEditing = true
+       
+        //pr10 task2 checking the ability to use the camera
+        print(UIImagePickerController.isSourceTypeAvailable(.camera))
+        if UIImagePickerController.isSourceTypeAvailable(.camera) == true {
+            picker.sourceType = .camera
+        }
         picker.delegate = self
         present(picker, animated: true)
     }
@@ -73,17 +79,43 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
         
-        let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
-        
-        ac.addTextField()
-        
-        ac.addAction(UIAlertAction(title: "OK", style: .default){ [weak self, weak ac] _ in
-            guard let newName = ac?.textFields?[0].text else { return }
-            person.name = newName
-            self?.collectionView.reloadData()
-        })
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        present(ac, animated: true)
+        //pr10 task1 add delay 1 sec for view alertcontroller and delete items from collectionview
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            
+            
+            let acMain = UIAlertController(title: "Options", message: "Choose to rename or delete the person", preferredStyle: .actionSheet)
+            
+            let renamePerson = UIAlertAction(title: "Rename", style: .default) { [weak self] (action) in
+                let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
+                ac.addTextField()
+                ac.addAction(UIAlertAction(title: "OK", style: .default){ [weak self, weak ac] _ in
+                    guard let newName = ac?.textFields?[0].text else { return }
+                    person.name = newName
+                    self?.collectionView.reloadData()
+                })
+                self?.present(ac, animated: true)
+            }
+            
+            
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { [weak self] (action) in
+                let ac = UIAlertController(title: "Confirm Delete", message: "Are you sure you would like to delete?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Delete", style: .destructive) { [weak self] (action) in
+                    self?.people.remove(at: indexPath.item)
+                    self?.collectionView.reloadData()
+                })
+                self?.present(ac, animated: true, completion: nil)
+            }
+            
+            
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            acMain.addAction(renamePerson)
+            acMain.addAction(deleteAction)
+            acMain.addAction(cancelAction)
+            self.present(acMain, animated: true, completion: nil)
+
+        }
     }
 }
 
