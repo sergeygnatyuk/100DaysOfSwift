ab54494aa92ed22501e61eead0484dbd7885eb18
Sergey <Starcom12@mail.ru>
2021-05-09 22:24:50 +0300

    Updated the tasks

--- 20 - Project15/Project15/Conrollers/ViewController.swift
+++ 20 - Project15/Project15/Conrollers/ViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     // Dependencies
     private var imageView: UIImageView!
-
+    
     // Properties
     private var currentAnimation = 0
     private let penguinImage = "penguin"
@@ -24,15 +24,46 @@ class ViewController: UIViewController {
         imageView.center = CGPoint(x: 512, y: 384)
         view.addSubview(imageView)
     }
-
+    
     //MARK: - Actions
     
-    @IBAction func tapped(_ sender: Any) {
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        //        UIView.animate(withDuration: 1, delay: 0, options: []) {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                break
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .green
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+            default:
+                break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        
         currentAnimation += 1
+        
         if currentAnimation > 7 {
             currentAnimation = 0
         }
     }
-    
 }
 
