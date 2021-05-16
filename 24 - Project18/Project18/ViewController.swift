a20c42ab3422fee1c021fc93a4e711c72d364eda
Sergey <Starcom12@mail.ru>
2021-05-16 20:57:18 +0300

    Started 18 project - debagging

---
+++ 24 - Project18/Project18/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Гнатюк Сергей on 16.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
