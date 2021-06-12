51b1a63fbd7d01451b6d13ec4addcf94dd94df57
Sergey <Starcom12@mail.ru>
2021-06-12 21:33:59 +0300

    Code style update

--- 3 - Project3/Project1/Controllers/DetailViewController.swift
+++ 3 - Project3/Project1/Controllers/DetailViewController.swift
@@ -7,29 +7,31 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+final class DetailViewController: UIViewController {
     
+    // UI
     @IBOutlet weak var imageView: UIImageView!
-    var selectedImage: String?
     
+    // Properties
+    var selectedImage: String?
     //Homework
     var selectedPictureNumber = 0
     var totalPictures = 0
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         title = "Pictures \(selectedPictureNumber) of \(totalPictures)"
         navigationItem.largeTitleDisplayMode = .never
-        
         //add icon action in navigationBar
- navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action,
-                                                            target: self, action: #selector(shareTaped))
-        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action,
+                                                            target: self,
+                                                            action: #selector(shareTaped))
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.hidesBarsOnTap = true
@@ -40,6 +42,7 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
+    // MARK: - @objc methods
     //method share image
     @objc func shareTaped() {
         guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
--- 3 - Project3/Project1/Controllers/ViewController.swift
+++ 3 - Project3/Project1/Controllers/ViewController.swift
@@ -7,56 +7,49 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+final class ViewController: UITableViewController {
     
+    // Properties
     var pictures = [String]()
     let cellReuseIdentifier = "Picture"
+    let titleName = "Storm Viewer"
+    let prefix = "nssl"
+    let fontName = "HiraMinProN-W3"
     let cellSpacingHeight: CGFloat = 7
     
-    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "Storm Viewer"
+        title = titleName
         navigationController?.navigationBar.prefersLargeTitles = true
-        
         let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        var items = try! fm.contentsOfDirectory(atPath: path)
-        
-        //sorting uitableviewcell
+        guard let path = Bundle.main.resourcePath else { return }
+        guard var items = try? fm.contentsOfDirectory(atPath: path) else { return }
         items.sort()
-        
         for item in items {
-            if item.hasPrefix("nssl") {
+            if item.hasPrefix(prefix) {
                 pictures.append(item)
             }
-            
         }
     }
     
+    // MARK: - TableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return pictures.count
-        
-        }
+        return pictures.count
+    }
     
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
-        
         cell.backgroundColor = UIColor.lightText
         cell.layer.borderColor = UIColor.cyan.cgColor
         cell.layer.borderWidth = 2.5
         cell.layer.cornerRadius = 15
         cell.clipsToBounds = true
-        
         //change font size in cells
-        cell.textLabel?.font = UIFont.init(name: "HiraMinProN-W3", size: 20)
-        
+        cell.textLabel?.font = UIFont.init(name: fontName, size: 20)
         return cell
-        }
-    
+    }
     //Delete cell
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -71,22 +64,18 @@ class ViewController: UITableViewController {
             //Homework
             viewController.selectedPictureNumber = indexPath.row + 1
             viewController.totalPictures = pictures.count
-            
             navigationController?.pushViewController(viewController, animated: true)
-            
         }
     }
-    
     //Customization cell
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return cellSpacingHeight
     }
+    
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView()
         headerView.backgroundColor = UIColor.clear
         return headerView
     }
-    
-
 }
 
